434c77ad9826eb3cee394f00830ced64d26c06c3
TheMateeus <105858907+TheMateeus@users.noreply.github.com>
2024-08-30 14:37:57 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
-window.addEventListener('DOMContentLoaded', event => {
+   async function handleCollapse(cardId, collapseId) {
+        var card = document.getElementById(cardId);
+        var collapse = document.getElementById(collapseId);
+        var cardImgTop = card.querySelector('.custom-card-img-top');
+
+        function updateCardImgTopStyle() {
+            if (collapse.classList.contains('show')) {
+                cardImgTop.classList.remove('expanded'); // Adiciona a classe 'expanded' se estiver visível
+            } else {
+                cardImgTop.classList.add('expanded'); // Remove a classe 'expanded' se não estiver visível
+            }
+        }
+
+        // Adiciona eventos para atualizar o estilo
+        collapse.addEventListener('shown.bs.collapse', updateCardImgTopStyle);
+        collapse.addEventListener('hidden.bs.collapse', updateCardImgTopStyle);
+        
+        // Inicializa o estado correto
+        updateCardImgTopStyle();
+    }
+    
+    window.addEventListener('DOMContentLoaded', (event) => {
+
+
 
     // Navbar shrink function
     var navbarShrink = function () {
@@ -20,23 +35,22 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
-
     };
 
     // Shrink the navbar 
     navbarShrink();
-
+    
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    //  Activate Bootstrap scrollspy on the main nav element
+    // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -50,5 +64,4 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
